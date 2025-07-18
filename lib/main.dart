9684cc7e6b2e9cfb4d9201810d0bd313ae6af9bb
Nick <daniel.vlaicu0512@gmail.com>
2025-07-18 10:51:39 +0300

    -android firebase stable version first

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/register_screen.dart';
@@ -11,7 +12,9 @@ import 'screens/manager/qr_generator_screen.dart';
 
 
 
-void main() {
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(RestaurantMenuApp());
 }
 
