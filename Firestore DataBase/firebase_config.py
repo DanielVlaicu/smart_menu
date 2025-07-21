990d72d91844f937ea1a0a0f2ca93d5067e02a35
NiceShot07 <165385524+NiceShot07@users.noreply.github.com>
2025-07-21 11:13:55 +0300

    Update firebase_config.py

--- Firestore DataBase/firebase_config.py
+++ Firestore DataBase/firebase_config.py
@@ -3,7 +3,14 @@ from firebase_admin import credentials
 import json
 import os
 
+
 def init_firebase():
-    service_account_info = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT"])
-    cred = credentials.Certificate(service_account_info)
+    if os.environ.get("FIREBASE_SERVICE_ACCOUNT"):
+        # Railway / Cloud
+        service_account_info = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT"])
+        cred = credentials.Certificate(service_account_info)
+    else:
+        # Local (Windows / PyCharm)
+        cred = credentials.Certificate("serviceAccountKey.json")
+
     firebase_admin.initialize_app(cred)
