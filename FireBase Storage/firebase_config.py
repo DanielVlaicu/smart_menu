000609859b4b600ee3f4747859b90d497d35621a
NiceShot07 <165385524+NiceShot07@users.noreply.github.com>
2025-07-24 13:49:52 +0300

    Update firebase_config.py

--- FireBase Storage/firebase_config.py
+++ FireBase Storage/firebase_config.py
@@ -2,7 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, storage, firestore
 
 def init_firebase():
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': 'smartmenu-d3e47.firebasestorage.app'
-    })
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("serviceAccountKey.json")
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': 'smartmenu-d3e47.firebasestorage.app'
+        })
